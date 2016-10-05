a604c269444592ea47f4607357290dcaeaea3948
anzileiro <anderson.anzileiro@gmail.com>
2016-10-05 11:07:04 -0700

    route app

--- routes/user.js
+++ routes/user.js
@@ -12,6 +12,13 @@ let routes = [
         method: 'get',
         path: '/v1/user/:id',
         handler: controller.get
+    },
+    {
+        method: 'get',
+        path: '/app',
+        handler: (_request, _response) => {
+            _response.status(200).json({ status: 'working', message: 'look https://github.com/anzileiro/hobbi-api' })
+        }
     }
 ]
 
